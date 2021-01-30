5b830cd52276a9d1bc1ef3f6bca6e75508296c69
rifqimfahmi <rifqi416@gmail.com>
2021-01-30 13:41:22 +0700

    refactor code

--- change_teams.py
+++ change_teams.py
@@ -4,16 +4,16 @@ from mitmproxy import http
 class ChangeTeam:
 
     def __init__(self):
-            self.modify_request = True
+            self.is_modify_request = True
 
     # mitmproxy event
     def response(self, flow: http.HTTPFlow) -> None:
-        if self.request_match(flow) and self.modify_request:
+        if self.is_request_match(flow) and self.is_modify_request:
             modified_response = open("./responses/long_name_teams.json", "r").read()
             flow.response.text = modified_response
             print("Change teams responses")
 
-    def request_match(self, flow: http.HTTPFlow) -> bool:
+    def is_request_match(self, flow: http.HTTPFlow) -> bool:
         request_path = flow.request.path
         request_query = flow.request.query
         return "lookup_all_teams" in request_path and request_query["id"] == "4328"
